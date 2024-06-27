df2ad955c694c0cdb97d35c9066d010a9c87b377
“Robert <robert@robertfabros.com>
2024-06-27 18:23:54 -0500

    Added Hierarchical Search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,20 @@ class HomeController < ApplicationController
 
   def search
     query = params[:q]
-    @parks = Park.where("name LIKE ?", "%#{query}%")
-    @school_zones = SchoolZoneSignage.joins(:school).where("schools.name LIKE ?", "%#{query}%")
-    @urban_trees = UrbanTree.where("common_name LIKE ?", "%#{query}%")
+    category = params[:category]
+
+    case category
+    when 'parks'
+      @results = Park.where("name LIKE ?", "%#{query}%")
+    when 'school_zones'
+      @results = SchoolZoneSignage.joins(:school).where("schools.name LIKE ?", "%#{query}%")
+    when 'urban_trees'
+      @results = UrbanTree.where("common_name LIKE ?", "%#{query}%")
+    else
+      @results = Park.where("name LIKE ?", "%#{query}%") +
+                 SchoolZoneSignage.joins(:school).where("schools.name LIKE ?", "%#{query}%") +
+                 UrbanTree.where("common_name LIKE ?", "%#{query}%")
+    end
 
     render :search
   end
