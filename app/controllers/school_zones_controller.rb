948d42ffbc278259dba866e52eaf59468d19a792
“Robert <robert@robertfabros.com>
2024-06-28 10:59:00 -0500

    Removed the school zones signage files for active records

--- app/controllers/school_zones_controller.rb
+++ app/controllers/school_zones_controller.rb
@@ -6,45 +6,5 @@ class SchoolZonesController < ApplicationController
     @school_zone_signage = SchoolZoneSignage.find(params[:id])
   end
 
-  def new
-    @school_zone_signage = SchoolZoneSignage.new
-  end
-
-  def edit
-  end
-
-  def create
-    @school_zone_signage = SchoolZoneSignage.new(school_zone_params)
-
-    if @school_zone_signage.save
-      redirect_to @school_zone_signage, notice: 'School zone signage was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @school_zone_signage.update(school_zone_params)
-      redirect_to @school_zone_signage, notice: 'School zone signage was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @school_zone_signage.destroy
-    redirect_to school_zones_url, notice: 'School zone signage was successfully destroyed.'
-  end
-
-  private
-
-  def set_school_zone_signage
-    @school_zone_signage = SchoolZoneSignage.find(params[:id])
-  end
-
-  def school_zone_params
-    params.require(:school_zone_signage).permit(:school_name, :zone_name, :sign_type, :image)
-  end
-
 
 end
